4b5f5f2cb2791e7286d6e8505d0a27b9c8f9f5e5
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-20 15:48:39 +0300

    Scripted auto-commit on change (2020-10-20 15:48:39) by gitwatch.sh

--- src/main/java/ru/Ivan/SparkJob.java
+++ src/main/java/ru/Ivan/SparkJob.java
@@ -14,7 +14,7 @@ public class SparkJob {
         JavaRDD<String> distOfAirportDelays = sc.textFile("664600583_T_ONTIME_sample.csv");
         JavaRDD<String> distOfAirportNames = sc.textFile("664600583_T_ONTIME_sample.csv");
 
-        JavaPairRDD<String, Long>
+        JavaPairRDD<String, Long> dataOfAiportNames = 
 
 
 
