50174c25505c1951f8d24c85a4007a9e62b2d05d
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-20 21:50:40 +0300

    Scripted auto-commit on change (2020-10-20 21:50:40) by gitwatch.sh

--- src/main/java/ru/Ivan/SparkJob.java
+++ src/main/java/ru/Ivan/SparkJob.java
@@ -75,7 +75,7 @@ public class SparkJob {
                     return new Tuple2<>(value._1(), FlightSerCount.toOutString(value._2()));
                 });
 
-        final Broadcast<Map<Integer, String>> broadcast = sc
+        final Broadcast<Map<Integer, String>> broadcast = sc.broadcast
 
 
 
