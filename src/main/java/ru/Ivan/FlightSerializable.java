149a131caff90b566fb74857adb22e3f77583e63
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-20 16:20:29 +0300

    Scripted auto-commit on change (2020-10-20 16:20:29) by gitwatch.sh

--- src/main/java/ru/Ivan/FlightSerializable.java
+++ src/main/java/ru/Ivan/FlightSerializable.java
@@ -9,4 +9,14 @@ public class FlightSerializable implements Serializable {
     private float arrDelay;
     private boolean cancelled;
 
+    public FlightSerializable() {}
+
+    public FlightSerializable(int destAirportID, int originAirportID, float arrDelay, boolean cancelled) {
+        this.destAirportID = destAirportID;
+        this.originAirportID = originAirportID;
+        this.arrDelay = arrDelay;
+        this.cancelled = cancelled;
+    }
+    
+
 }
