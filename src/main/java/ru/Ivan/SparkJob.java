0fda19b094bd3edb6e3e0474b69fce7af73b92b3
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-20 14:57:59 +0300

    Scripted auto-commit on change (2020-10-20 14:57:59) by gitwatch.sh

--- src/main/java/ru/Ivan/SparkJob.java
+++ src/main/java/ru/Ivan/SparkJob.java
@@ -1,4 +1,9 @@
 package ru.Ivan;
 
 public class SparkJob {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf().setAppName("lab3");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+    }
 }
