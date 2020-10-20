2d83e3bfb99c4790eed9df84e92a6c220cffd0ec
Ivan Chernyshov <chaos.ivan@yandex.ru>
2020-10-20 15:03:07 +0300

    Scripted auto-commit on change (2020-10-20 15:03:07) by gitwatch.sh

--- src/main/java/ru/Ivan/SparkJob.java
+++ src/main/java/ru/Ivan/SparkJob.java
@@ -1,6 +1,7 @@
 package ru.Ivan;
 
 import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 
 public class SparkJob {
@@ -8,5 +9,7 @@ public class SparkJob {
     public static void main(String[] args) {
         SparkConf conf = new SparkConf().setAppName("lab3");
         JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> distOfDelays = sc.textFile()
     }
 }
